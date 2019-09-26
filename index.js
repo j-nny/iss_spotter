1c4cff9476ae85cf356d78df2a6f012ee1a5e1f4
Jenny Nguy <j.nguy809@gmail.com>
2019-09-26 23:58:33 +0000

    fetched next iss flyover times

--- index.js
+++ index.js
@@ -1,28 +1,35 @@
-const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require('./iss');
+const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes, nextISSTimesForMyLocation } = require('./iss');
 
-fetchMyIP((error, ip) => {
-  if (error) {
-    console.log('It didn\'t work!', error);
-    return;
-  } else {
-    console.log('It worked! Returned IP' , ip);
-  }
-});
+// fetchMyIP((error, ip) => {
+//   if (error) {
+//     console.log('It didn\'t work!', error);
+//     return;
+//   } else {
+//     console.log('It worked! Returned IP' , ip);
+//   }
+// });
 
-fetchCoordsByIP('162.245.144.188', (error, coords) => {
-  if (error) {
-    console.log('Error: ', error);
-    return;
-  } else {
-  console.log(coords);
-  }
-});
+// fetchCoordsByIP('162.245.144.188', (error, coords) => {
+//   if (error) {
+//     console.log('Error: ', error);
+//     return;
+//   } else {
+//   console.log(coords);
+//   }
+// });
 
-fetchISSFlyOverTimes( { latitude: '49.26200', longitude: '-123.09230' }, (error, response) => {
+// fetchISSFlyOverTimes( { latitude: '49.26200', longitude: '-123.09230' }, (error, response) => {
+//   if (error) {
+//     console.log('Error: ', error);
+//     return;
+//   } else {
+//   console.log(response);
+//   }
+// });
+
+nextISSTimesForMyLocation((error, passTimes) => {
   if (error) {
-    console.log('Error: ', error);
-    return;
-  } else {
-  console.log(response);
+    return console.log('It didn\'t work! ', error)
   }
-});
+  console.log(passTimes);
+})
