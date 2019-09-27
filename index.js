b02179ad4325fc8028ef9ccad13ec1ade268130d
Jenny Nguy <j.nguy809@gmail.com>
2019-09-27 00:19:58 +0000

    converted unix to utc dates

--- index.js
+++ index.js
@@ -27,9 +27,19 @@ const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes, nextISSTimesForMyLocat
 //   }
 // });
 
+const printPassTimes = function(passTimes) {
+  for (let pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`)
+  }
+}
+
+
 nextISSTimesForMyLocation((error, passTimes) => {
   if (error) {
     return console.log('It didn\'t work! ', error)
   }
-  console.log(passTimes);
-})
+  printPassTimes(passTimes);
+});
